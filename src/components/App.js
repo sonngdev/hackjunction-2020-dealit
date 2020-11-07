7716355a987a863b494a8bbe4be2097da9ff5e6c
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-11-07 16:55:37 +0700

    feat: create ListView

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import ListView from './ListView';
+import store from '../redux/store';
 
 function App() {
   return (
     <Provider store={store}>
-      <div>Hello world</div>
+      <ListView />
     </Provider>
   );
 }
---
+++ src/components/ListView/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ListView() {
+  return (
+    <div>
+      ListView
+    </div>
+  );
+}
+
+export default ListView;
