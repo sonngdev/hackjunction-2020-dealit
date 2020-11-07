c866b6132fd3563d8f93b216b7b6b1c03afdb209
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-11-07 20:51:45 +0700

    feat: render header in List view

---
+++ src/components/ListView/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <div>Good morning,</div>
+      <div>Junction Connected</div>
+      <img src="http://via.placeholder.com/150" alt="Avatar" />
+    </div>
+  );
+}
+
+export default Header;
--- src/components/ListView/index.js
+++ src/components/ListView/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getListProducts } from 'utils/request.mock';
+import Header from './Header';
 import SuggestionModal from './SuggestionModal';
 import PersonalList from './PersonalList';
 
@@ -20,12 +21,18 @@ function ListView() {
 
   return (
     <div>
-      I&apos;m in need of
-      <button type="button" onClick={showModal}>
-        +
-      </button>
-      {showSuggestionModal && <SuggestionModal />}
-      <PersonalList listProducts={listProducts} />
+      <div>
+        <Header />
+      </div>
+
+      <div>
+        I&apos;m in need of
+        <button type="button" onClick={showModal}>
+          +
+        </button>
+        {showSuggestionModal && <SuggestionModal />}
+        <PersonalList listProducts={listProducts} />
+      </div>
     </div>
   );
 }
