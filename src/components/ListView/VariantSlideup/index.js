4e8af46ab9664f0761e8751019198bb1c71cb132
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-11-08 08:17:09 +0700

    feat: variant selection

--- src/components/ListView/VariantSlideup/index.js
+++ src/components/ListView/VariantSlideup/index.js
@@ -1,11 +1,14 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import Button from 'components/Shared/Button';
 
 function VariantSlideup({ closeSlideup }) {
+  const [selectedVariant, setSelectedVariant] = useState(null);
+
   const backgroundRef = useRef();
   const hideSlideup = (e) => {
     if (e.target === backgroundRef.current) closeSlideup();
   };
+
   return (
     <div
       className="h-screen w-screen absolute top-0 bottom-0 right-0 left-0 flex flex-col-reverse"
@@ -21,6 +24,46 @@ function VariantSlideup({ closeSlideup }) {
           className="rounded-full w-10 h-1 m-auto mb-6"
           style={{ backgroundColor: '#CECECE' }}
         />
+
+        <div className="flex items-center">
+          <img
+            src="http://via.placeholder.com/150"
+            className="w-8 h-8 object-contain"
+            alt="Shampoo"
+          />
+          <div>Shampoo</div>
+        </div>
+
+        <div className="my-2">
+          {[
+            'For top load washer',
+            'Example abc',
+            'For front load washer',
+            'Example def',
+          ]
+            .map((n, i) => ({
+              id: i,
+              name: n,
+            }))
+            .map((variant) =>
+              selectedVariant?.id === variant.id ? (
+                <button
+                  className="text-purple border border-gray-300 bg-gray-300 rounded-full px-3 py-2 inline-block mr-3 mb-3"
+                  onClick={() => setSelectedVariant(null)}
+                >
+                  {variant.name}
+                </button>
+              ) : (
+                <button
+                  className="text-purple border border-purple rounded-full px-3 py-2 inline-block opacity-50 mr-3 mb-3"
+                  onClick={() => setSelectedVariant(variant)}
+                >
+                  {variant.name}
+                </button>
+              ),
+            )}
+        </div>
+
         <Button>Save</Button>
       </div>
     </div>
