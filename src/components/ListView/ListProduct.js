66a15e113014f1a6e906fed641595228ec1b1870
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-11-08 09:18:09 +0700

    feat: render variant in list product

--- src/components/ListView/ListProduct.js
+++ src/components/ListView/ListProduct.js
@@ -13,17 +13,32 @@ function ListProduct({
   };
 
   return (
-    <div className="bg-white rounded-lg flex justify-between items-center p-4">
-      <div className="flex items-center">
-        <img src={imageUrl} className="w-8 h-8 object-contain" alt={name} />
-        <div>{name}</div>
+    <div className="bg-white rounded-lg p-4">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center">
+          <img src={imageUrl} className="w-8 h-8 object-contain" alt={name} />
+          <div>{name}</div>
+        </div>
+
+        {variants && variants.length && (
+          <button className="text-purple text-sm" onClick={showVariantSlideup}>
+            Select variants
+          </button>
+        )}
       </div>
 
-      {variants && variants.length && (
-        <button className="text-purple text-sm" onClick={showVariantSlideup}>
-          Select variants
-        </button>
-      )}
+      <div className="ml-8">
+        {variants
+          .filter((v) => v.selected)
+          .map((v) => (
+            <div
+              key={v.id}
+              className="px-3 py-1 bg-gray-300 rounded-full mr-2 mt-2 text-xs inline-block text-purple"
+            >
+              {v.name}
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
